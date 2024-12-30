b1203a738080760c01c6e571849cf5d9e6741ff6
Mohamed Elaouan <138141267+mohamed-elaouan@users.noreply.github.com>
2024-12-30 23:57:11 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT =  4000; process.env.PORT ||
+const PORT =  process.env.PORT ||5500; 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
