37b53ba8cc813b3550bfd181644465bd1bd8dea3
Mohamed Elaouan <138141267+mohamed-elaouan@users.noreply.github.com>
2024-12-31 07:53:21 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -9,3 +9,8 @@ const PORT =  process.env.PORT ||5500;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+// for run it in the docker :
+// docker container run -itd -p 5500:5500 --name mynodeApp mohamedelaouan/my_node_app:v1.0
